c6719185bf29ce5b6aabb53e0fe57a59df9e2ca0
Роберт Амангильдин <AmangildinRM@stud.kai.ru>
2022-11-28 22:07:47 +0300

    что-то типа

---
+++ Class2.cs
@@ -0,0 +1,36 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Compare compare = new Compare();
+        compare.toMatch();
+    }
+}
+
+class Compare
+{
+    public string toMatch ()
+    {
+        double d1;
+        double d2;
+        Console.WriteLine("Введите первое число: ");
+        d1= Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число: ");
+        d2= Convert.ToDouble(Console.ReadLine()); 
+        
+        if (d1 > d2)
+        {
+            return "Первое число больше второго";
+        }  
+        else if (d1 < d2)
+        {
+            return "Первое число меньше второго";
+        }
+        else
+        {
+            return "Числа равны";
+        }
+    }
+}
